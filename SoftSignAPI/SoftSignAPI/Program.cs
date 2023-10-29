4d566d9e6f1353fbef215a164188a8112b8eb540
ultimaXDeath <ultimatexdeath01@gmail.com>
2023-10-29 15:22:16 +0300

    update Program.cs

--- SoftSignAPI/SoftSignAPI/Program.cs
+++ SoftSignAPI/SoftSignAPI/Program.cs
@@ -1,22 +1,109 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SoftSignAPI.Context;
+using SoftSignAPI.Helpers;
+using SoftSignAPI.Interfaces;
+using SoftSignAPI.Repositories;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//Scope
+builder.Services.AddScoped<ISocietyRepository, SocietyRepository>();
+builder.Services.AddScoped<IOfferRepository, OfferRepository>();
+builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
+builder.Services.AddScoped<IFieldRepository, FieldRepository>();
+builder.Services.AddScoped<IUserDocumentRepository, UserDocumentRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization Bearer (\"bearer {token}\"",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddHttpContextAccessor();
+
+#region Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "SoftSign",
+        policy =>
+        {
+            policy.WithOrigins("*", "*").AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed(origin => true).AllowCredentials();
+        });
+});
+#endregion
+
+#region JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Tools.MySecret)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+#endregion
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+#region dbContext
+builder.Services.AddDbContext<dbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("dbContext"));
+    /*sqlServerOptionsAction: sqlOption =>
+	{
+		sqlOption.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(5), errorNumbersToAdd: null);
+	});
+	options.EnableSensitiveDataLogging();*/
+});
+
+
+#endregion
 
 var app = builder.Build();
 
+#region Cors
+app.UseCors("SoftSign");
+#endregion
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
+
+#region JWT
+app.UseAuthentication();
+#endregion
 
 app.UseAuthorization();
 
