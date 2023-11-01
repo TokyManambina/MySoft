5de5632e1e055d17398c39889f19736160a0008c
MikaloMSI3 <mikaloratongaranto@gmail.com>
2023-11-01 20:26:57 +0300

    add fieldController

---
+++ SoftSignAPI/SoftSignAPI/Dto/FieldDto.cs
@@ -0,0 +1,20 @@
+﻿using SoftSignAPI.Model;
+
+namespace SoftSignAPI.Dto
+{
+    public class FieldDto
+    {
+        public int? Id { get; set; }
+        public string? Variable { get; set; }
+        public string? Page { get; set; }
+        public double? X { get; set; }
+        public double? Y { get; set; }
+        public double? Width { get; set; }
+        public double? Height { get; set; }
+        public double? PDF_Width { get; set; }
+        public double? PDF_Height { get; set; }
+        public FieldType? FieldType { get; set; }
+        public string? Detail { get; set; }
+        public string? Text { get; set; }
+    }
+}
